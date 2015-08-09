04352d49e82b2c113730f07662fd6befe8221563
Jason Walkow <jason.walkow1@gmail.com>
2015-08-08 20:11:21 -0700

    commit finish

--- js/app.js
+++ js/app.js
@@ -1,16 +1,24 @@
 $(document).ready(function() {
+	doIntro();
+	$('.instructions').hide();
 	$('.ryu').mouseenter(function() {
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
+		$('.logo').hide();
+		$('.instructions').show();
 	})
 	.mouseleave(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
+		$('.instructions').hide();
+		$('.logo').show();
 	})
 	.mousedown(function() {
 		playHadouken();
 		$('.ryu-ready').hide();
+		$('.instructions').hide();
 		$('.ryu-throwing').show();
+		$('.logo').hide();
 		$('.hadouken').finish().show()
   			.animate(
     			{'left': '1020px'},
@@ -27,8 +35,31 @@ $(document).ready(function() {
 	});
 });
 
+$(document).keydown(function(e) {
+		if(e.which == 88) {
+		$('.ryu-still').hide();
+		$('.ryu-ready').hide();
+		$('.instructions').hide();
+		$('.logo').show();
+		$('.ryu-cool').show();
+	}
+});
+
+$(document).keyup(function(e) {
+		if(e.which == 88) {
+		$('.ryu-cool').hide();	
+		$('.ryu-still').show();
+	}
+});
+
 function playHadouken () {
   	$('#hadouken-sound')[0].volume = 0.5;
   	$('#hadouken-sound')[0].load();
   	$('#hadouken-sound')[0].play();
+}
+
+function doIntro() {
+  $('#theme-song')[0].volume = 0.2;
+  $('#theme-song')[0].load();
+  $('#theme-song')[0].play();
 }
