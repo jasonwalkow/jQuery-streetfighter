1538dce32f30f63402d18a9bcf62cd3a9f1ab346
Jason Walkow <jason.walkow1@gmail.com>
2015-08-08 18:27:52 -0700

    commit layout

---
+++ js/app.js
@@ -0,0 +1,19 @@
+//Ryu still position
+
+//Listen for mouseenter and mouseleave events on Ryu
+
+//Mouseenter moves Ryu into ready position
+
+//Mouseleave moves Ryu back into still position
+
+//Listen for click events
+
+//Mousedown initiates Hadouken sequence
+
+//Mouseup ends Hadouken sequence
+
+//Listen for key events
+
+//Keydown initiates looking cool sequence
+
+//Keyup ends looking cool sequence
